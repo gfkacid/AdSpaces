0c1f1615863646c592e218a14763754109a4da74
mme <9083787+mme022@users.noreply.github.com>
2022-09-19 17:00:54 +0100

    contract calling for new adspace now working

--- front/src/views/admin/default/components/UserAdSpaces.js
+++ front/src/views/admin/default/components/UserAdSpaces.js
@@ -41,9 +41,8 @@ import {
 } from "react-table";
 import { BigNumber } from "ethers";
 
-
 import { useForm } from "react-hook-form";
-  
+
 // Custom components
 import { isNumeric } from "@chakra-ui/utils";
 
@@ -60,10 +59,6 @@ import abi from "../variables/AdSpaceFactory.json";
 export default function UserAdSpaces(props) {
   const { columnsData, tableData } = props;
 
-  //wagmi stuff
-  const contractABI = abi.abi;
-  const contractAddress = abi.address;
-  
   const columns = useMemo(() => columnsData, [columnsData]);
   const data = useMemo(() => tableData, [tableData]);
 
@@ -74,11 +69,11 @@ export default function UserAdSpaces(props) {
     },
     useGlobalFilter,
     useSortBy,
-  usePagination)
+    usePagination
+  );
 
-  
-    const textColor = useColorModeValue("secondaryGray.900", "white");
-    const borderColor = useColorModeValue("gray.200", "whiteAlpha.100");
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const borderColor = useColorModeValue("gray.200", "whiteAlpha.100");
 
   const {
     getTableProps,
@@ -90,170 +85,198 @@ export default function UserAdSpaces(props) {
   } = tableInstance;
   initialState.pageSize = 5;
   // modal
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    // New AdSpace form
-    const { register, handleSubmit, formState: { errors } } = useForm();
-    const [NumNFTs, setNumNFTs] = React.useState('10')
-    const [price, setPrice] = React.useState('0.55')
-    
-    // submit New AdSpace Form
-    const onSubmit = data => {
-      console.log(data);
-// AdSpaceFactory on Optimism Goerli
-const { config: newAdSpaceConfig } = usePrepareContractWrite({
-  addressOrName: contractAddress,
-  contractInterface: contractABI,
-  functionName: "createAdSpace",
-  args: [data.name, data.website,data.price, BigNumber.from(data.numNFTs), data.size],
-});
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  // New AdSpace form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [NumNFTs, setNumNFTs] = React.useState("10");
+  const [price, setPrice] = React.useState("0.55");
+  const [website, setWebsite] = React.useState("0");
+  const [name, setName] = React.useState("0");
+  const [size, setSize] = React.useState("0");
 
-const {
-  data: writeData,
-  isLoading,
-  isSuccess,
-  write: newAdSpace,
-} = useContractWrite(newAdSpaceConfig);
-    }
+  //wagmi/web3 stuff
+  const contractABI = abi.abi;
+  const contractAddress = abi.address;
+  const { address: userAddress, isConnected } = useAccount();
+  const { config: newAdSpaceConfig } = usePrepareContractWrite({
+    addressOrName: contractAddress,
+    contractInterface: contractABI,
+    functionName: "createAdSpace",
+    args: [
+      name,
+      website,
+      parseInt(price).toString(),
+      BigNumber.from(NumNFTs),
+      size,
+    ],
+  });
+  const {
+    data: writeData,
+    isLoading,
+    isSuccess,
+    write: newAdSpace,
+  } = useContractWrite(newAdSpaceConfig);
 
-    const validateNumNFTs = num => {
-      if(!isNumeric(num))return 1;
-      return (num >= 1 && num <= 20) ? num : NumNFTs;
-    }
+  // submit New AdSpace Form
+  const onSubmit = (data) => {
+    newAdSpace({
+      recklesslySetUnpreparedArgs: [
+        data.name,
+        data.website,
+        parseInt(data.price * 100).toString(),
+        BigNumber.from(data.numNFTs),
+        data.size,
+      ],
+    });
+  };
 
-    const { address: userAddress, isConnected } = useAccount();
+  const validateNumNFTs = (num) => {
+    if (!isNumeric(num)) return 1;
+    return num >= 1 && num <= 20 ? num : NumNFTs;
+  };
 
-
-  
-    return (
-      <Card
-        direction='column'
-        w='100%'
-        px='0px'
-        overflowX={{ sm: "scroll", lg: "hidden" }}>
-        <Flex px='25px' justify='space-between' mb='10px' align='center'>
-          <Text
-            color={textColor}
-            fontSize='22px'
-            fontWeight='700'
-            lineHeight='100%'>
-            My AdSpaces
-          </Text>
-          <Button colorScheme="brand" variant="solid" onClick={onOpen}>+ NEW</Button>
-        </Flex>
-        <Table {...getTableProps()} variant='simple' color='gray.500' mb='24px'>
-          <Thead>
-            {headerGroups.map((headerGroup, index) => (
-              <Tr {...headerGroup.getHeaderGroupProps()} key={index}>
-                {headerGroup.headers.map((column, index) => (
-                  <Th
-                    {...column.getHeaderProps(column.getSortByToggleProps())}
-                    pe='10px'
-                    key={index}
-                    borderColor={borderColor}>
-                    <Flex
-                      justify='space-between'
-                      align='center'
-                      fontSize={{ sm: "10px", lg: "12px" }}
-                      color='gray.400'>
-                      {column.render("Header")}
-                    </Flex>
-                  </Th>
-                ))}
-              </Tr>
-            ))}
-          </Thead>
-          <Tbody {...getTableBodyProps()}>
-            {page.map((row, index) => {
-              prepareRow(row);
-              return (
-                <Tr {...row.getRowProps()} key={index}>
-                  {row.cells.map((cell, index) => {
-                    let data = "";
-                    if (cell.column.id === "name") {
-                      data = (
-                        <Text color={textColor} fontSize='sm' fontWeight='700'>
-                          <Link
-                            href={'/#/admin/adspace/'+row.original.id}
-                            >
-                            {cell.value}
-                          </Link>
-                        </Text>
-                      );
-                    } else if (cell.column.id === "size") {
-                      data = (
-                        <SizeIcon size={cell.value} />
-                      );
-                    } else if (cell.column.id === "price") {
-                      data = (
-                        <Text color={textColor} fontSize='sm' fontWeight='700'>
-                          ${cell.value}
-                        </Text>
-                      );
-                    } else if (cell.column.id === "website") {
-                      data = (
-                        <Text color={textColor} fontSize='sm' fontWeight='700'>
-                          <Link
-                            href={cell.value}
-                            target='_blank'
-                            >
-                            {cell.value}
-                          </Link>
-                        </Text>
-                      );
-                    }else if(cell.column.id === "status") {
-                        data = (
-                          <AdSpaceStatus status={cell.value}  textColor={textColor}/>
-                        );
-                    }else if(cell.column.id === "verified") {
-                      data = (
-                        <VerifiedStatusIcon status={cell.value} />
-                      );
-                    }
-                    return (
-                      <Td
-                        {...cell.getCellProps()}
-                        key={index}
-                        fontSize={{ sm: "14px" }}
-                        maxH='30px !important'
-                        py='8px'
-                        minW={{ sm: "150px", md: "200px", lg: "auto" }}
-                        borderColor='transparent'>
-                        {data}
-                      </Td>
+  return (
+    <Card
+      direction="column"
+      w="100%"
+      px="0px"
+      overflowX={{ sm: "scroll", lg: "hidden" }}
+    >
+      <Flex px="25px" justify="space-between" mb="10px" align="center">
+        <Text
+          color={textColor}
+          fontSize="22px"
+          fontWeight="700"
+          lineHeight="100%"
+        >
+          My AdSpaces
+        </Text>
+        <Button colorScheme="brand" variant="solid" onClick={onOpen}>
+          + NEW
+        </Button>
+      </Flex>
+      <Table {...getTableProps()} variant="simple" color="gray.500" mb="24px">
+        <Thead>
+          {headerGroups.map((headerGroup, index) => (
+            <Tr {...headerGroup.getHeaderGroupProps()} key={index}>
+              {headerGroup.headers.map((column, index) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  pe="10px"
+                  key={index}
+                  borderColor={borderColor}
+                >
+                  <Flex
+                    justify="space-between"
+                    align="center"
+                    fontSize={{ sm: "10px", lg: "12px" }}
+                    color="gray.400"
+                  >
+                    {column.render("Header")}
+                  </Flex>
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {page.map((row, index) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()} key={index}>
+                {row.cells.map((cell, index) => {
+                  let data = "";
+                  if (cell.column.id === "name") {
+                    data = (
+                      <Text color={textColor} fontSize="sm" fontWeight="700">
+                        <Link href={"/#/admin/adspace/" + row.original.id}>
+                          {cell.value}
+                        </Link>
+                      </Text>
                     );
-                  })}
-                </Tr>
-              );
-            })}
-          </Tbody>
-        </Table>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>New AdSpace</ModalHeader>
-            <ModalCloseButton />
-            <form>
+                  } else if (cell.column.id === "size") {
+                    data = <SizeIcon size={cell.value} />;
+                  } else if (cell.column.id === "price") {
+                    data = (
+                      <Text color={textColor} fontSize="sm" fontWeight="700">
+                        ${cell.value}
+                      </Text>
+                    );
+                  } else if (cell.column.id === "website") {
+                    data = (
+                      <Text color={textColor} fontSize="sm" fontWeight="700">
+                        <Link href={cell.value} target="_blank">
+                          {cell.value}
+                        </Link>
+                      </Text>
+                    );
+                  } else if (cell.column.id === "status") {
+                    data = (
+                      <AdSpaceStatus
+                        status={cell.value}
+                        textColor={textColor}
+                      />
+                    );
+                  } else if (cell.column.id === "verified") {
+                    data = <VerifiedStatusIcon status={cell.value} />;
+                  }
+                  return (
+                    <Td
+                      {...cell.getCellProps()}
+                      key={index}
+                      fontSize={{ sm: "14px" }}
+                      maxH="30px !important"
+                      py="8px"
+                      minW={{ sm: "150px", md: "200px", lg: "auto" }}
+                      borderColor="transparent"
+                    >
+                      {data}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>New AdSpace</ModalHeader>
+          <ModalCloseButton />
+          <form>
             <ModalBody>
               <FormControl isRequired>
                 <FormLabel>Name</FormLabel>
-                  <Input placeholder='mysite.com | footer' {...register('name')}></Input>
+                <Input
+                  placeholder="mysite.com | footer"
+                  {...register("name")}
+                ></Input>
               </FormControl>
               <FormControl isRequired mt={4}>
                 <FormLabel>Website</FormLabel>
-                  <Input type='url' placeholder='https://myshop.com' {...register('website')}></Input>
+                <Input
+                  type="url"
+                  placeholder="https://myshop.com"
+                  {...register("website")}
+                ></Input>
               </FormControl>
               <FormControl isRequired mt={4}>
                 <FormLabel>Banner Size</FormLabel>
-                <Select placeholder='Select size' {...register('size')}>
-                  <option value='wide'>Wide | 728 x 90 px</option>
-                  <option value='skyscraper'>Skyscraper | 160 x 600 px</option>
-                  <option value='square'>Campaign 3 | 200 x 200 px</option>
+                <Select placeholder="Select size" {...register("size")}>
+                  <option value="wide">Wide | 728 x 90 px</option>
+                  <option value="skyscraper">Skyscraper | 160 x 600 px</option>
+                  <option value="square">Campaign 3 | 200 x 200 px</option>
                 </Select>
               </FormControl>
               <FormControl isRequired mt={4}>
                 <FormLabel>Asking Price / hour - $USDC</FormLabel>
                 <NumberInput
-                  {...register('price')}
+                  {...register("price")}
                   onChange={(valueString) => setPrice(valueString)}
                   value={price}
                   step={0.01}
@@ -266,13 +289,17 @@ const {
                     <NumberDecrementStepper />
                   </NumberInputStepper>
                 </NumberInput>
-                <Text as='i' size='sm'>minimum: $0.01</Text>
+                <Text as="i" size="sm">
+                  minimum: $0.01
+                </Text>
               </FormControl>
               <FormControl isRequired mt={4}>
                 <FormLabel>Revenue Share NFTs</FormLabel>
                 <NumberInput
-                 {...register('numNFTs')}
-                  onChange={(valueString) => setNumNFTs(validateNumNFTs(valueString))}
+                  {...register("numNFTs")}
+                  onChange={(valueString) =>
+                    setNumNFTs(validateNumNFTs(valueString))
+                  }
                   value={NumNFTs}
                   step={1}
                   min={1}
@@ -284,18 +311,26 @@ const {
                     <NumberDecrementStepper />
                   </NumberInputStepper>
                 </NumberInput>
-                <Text as='i' size='sm'>20 NFTs max</Text>
+                <Text as="i" size="sm">
+                  20 NFTs max
+                </Text>
               </FormControl>
             </ModalBody>
             <ModalFooter>
               <Button mr={3} onClick={onClose}>
                 Close
               </Button>
-              <Button onClick={handleSubmit(onSubmit)} colorScheme="brand" variant="solid">Submit AdSpace</Button>
+              <Button
+                onClick={handleSubmit(onSubmit)}
+                colorScheme="brand"
+                variant="solid"
+              >
+                Submit AdSpace
+              </Button>
             </ModalFooter>
-            </form>
-          </ModalContent>
-        </Modal>
-      </Card>
-    );
-  }
+          </form>
+        </ModalContent>
+      </Modal>
+    </Card>
+  );
+}
