aedfc330c46a41254b9e7be2c116e7e979ffe915
Krisztian Barta <bartariki@gmail.com>
2022-09-24 00:28:47 +0200

    web2API

--- front/src/views/admin/default/components/UserAdSpaces.js
+++ front/src/views/admin/default/components/UserAdSpaces.js
@@ -54,11 +54,8 @@ import VerifiedStatusIcon from "components/domain/VerifiedStatusIcon";
 import { useAccount, useContractWrite, usePrepareContractWrite } from "wagmi";
 //import { InjectedConnector } from "wagmi/connectors/injected";
 import abi from "../variables/AdSpaceFactory.json";
-<<<<<<< HEAD
 // here we need the AdSpace ABI + we need the DaIContract ABI
-=======
 import DAIicon from "components/domain/DAIicon";
->>>>>>> 435aa8aeacb317f7c69c055ece237fff52f8a05f
 
 // Assets
 export default function UserAdSpaces(props) {
