4a3404d7544330a4d85397ef32bcdfcb4ed82758
mme <9083787+mme022@users.noreply.github.com>
2022-09-25 13:14:04 +0100

    added nft holder table on adspace listing

--- front/src/views/admin/adspace/index.jsx
+++ front/src/views/admin/adspace/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 // Chakra imports
 import {
   Box,
@@ -24,6 +25,15 @@ import {
   ModalCloseButton,
   useColorModeValue,
   useDisclosure,
+  Table,
+  TableContainer,
+  TableCaption,
+  Td,
+  Tr,
+  Thead,
+  Th,
+  Tbody,
+  Tfoot,
 } from "@chakra-ui/react";
 
 import Card from "components/card/Card.js";
@@ -40,7 +50,8 @@ import { ethers, BigNumber } from "ethers";
 import { useAccount, useSigner } from "wagmi";
 import DAIicon from "components/domain/DAIicon";
 import DAItokenABI from "../../../variables/DaiTokenABI.json";
-import AdSpaceJson from "../../../variables/AdSpace.json"
+import AdSpaceJson from "../../../variables/AdSpace.json";
+import { WalletIcon } from "components/icons/Icons";
 
 export default function AdSpaceListing() {
   // AdSpace
@@ -52,7 +63,8 @@ export default function AdSpaceListing() {
 
   const [newDealDuration, setNewDealDuration] = React.useState("1");
   const [userCampaigns, setUserCampaigns] = useState([]);
-  const [selectedCampaign, setSelectedCampaign] = useState(null)
+  const [selectedCampaign, setSelectedCampaign] = useState(null);
+  const [tokenOwners, setTokenOwners] = useState([]);
 
   const DAIcontract = new ethers.Contract(
     "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1",
@@ -76,7 +88,7 @@ export default function AdSpaceListing() {
   const networkConfig = getTableLandConfig();
   const adspaceTable = TablelandTables["AdSpaces"];
   const campaignsTable = TablelandTables["Campaigns"];
-  
+
   // Loads the AdSpace
   async function fetchAdSpace() {
     const tablelandConnection = await connect({
@@ -90,7 +102,7 @@ export default function AdSpaceListing() {
     const result = await resultsToObjects(fetchAdSpaceQuery);
     return result[0];
   }
-  
+
   // Loads user's Campaigns
   async function fetchUserCampaigns() {
     const tablelandConnection = await connect({
@@ -109,19 +121,16 @@ export default function AdSpaceListing() {
   // Checks if user has approved this AdSpace's contract to transfer DAI on this behalf
   const checkDAIApproved = async (_contractAddress) => {
     const response = await DAIcontract.allowance(address, _contractAddress);
-    const allowance = BigNumber.from(response.toString())
-    const bool = allowance.gt(0)
+    const allowance = BigNumber.from(response.toString());
+    const bool = allowance.gt(0);
     console.log(bool);
     return bool;
   };
 
   // Prompts user to approve this AdSpace's contract to transfer DAI on this behalf
   const approveDAI = async () => {
-    const num = ethers.utils.parseEther('100000');
-    const response = await DAIcontract.approve(
-      AdSpace.contract,
-      num
-    );
+    const num = ethers.utils.parseEther("100000");
+    const response = await DAIcontract.approve(AdSpace.contract, num);
     console.log(response);
   };
 
@@ -132,23 +141,47 @@ export default function AdSpaceListing() {
       AdSpaceJson.abi,
       signer
     );
-    
-    const amountTotal = ethers.utils.parseEther((newDealDuration * AdSpace.asking_price).toString()) 
-    console.log(amountTotal)
-    const response = await AdSpaceContract.createDeal(amountTotal, newDealDuration, selectedCampaign);
-    console.log(response)
-  }
+
+    const amountTotal = ethers.utils.parseEther(
+      (newDealDuration * AdSpace.asking_price).toString()
+    );
+    //console.log(amountTotal);
+    const response = await AdSpaceContract.createDeal(
+      amountTotal,
+      newDealDuration,
+      selectedCampaign
+    );
+    //console.log(response);
+  };
+
+  const fetchTokenOwners = async () => {
+    const AdSpaceContract = new ethers.Contract(
+      AdSpace.contract,
+      AdSpaceJson.abi,
+      signer
+    );
+    const tempArr = [];
+    const numNFTs = await AdSpaceContract.totalSupply();
+
+    for (let i = 1; i <= numNFTs; i++) {
+      const tempAddr = await AdSpaceContract.ownerOf(i);
+      if (tempAddr) {
+        tempArr.push(tempAddr);
+      }
+    }
+    return tempArr;
+  };
 
   // populate this page's AdSpace
   useEffect(() => {
     fetchAdSpace()
       .then((AdSpaceRes) => {
-        AdSpaceRes.asking_price = (AdSpaceRes.asking_price / 100).toFixed(2)
+        AdSpaceRes.asking_price = (AdSpaceRes.asking_price / 100).toFixed(2);
         setAdSpace(AdSpaceRes);
-         
+
         // fetch user's campaigns to populate the [ Create Deal ] Modal
         fetchUserCampaigns().then((CampaignsRes) => {
-          console.log(CampaignsRes)
+          //console.log(CampaignsRes);
           setUserCampaigns(CampaignsRes);
         });
       })
@@ -157,21 +190,29 @@ export default function AdSpaceListing() {
       });
   }, []);
 
-
   //Here we can read DAI contract whether the user has allowed the AdSpace contract to spend user's DAI
   useEffect(() => {
-    if(AdSpace?.contract){
-      checkDAIApproved(AdSpace.contract).then( (bool) => {
-        console.log('in')
+    if (AdSpace?.contract) {
+      checkDAIApproved(AdSpace.contract).then((bool) => {
         setIsApproved(bool);
       });
     }
-    
   }, [AdSpace]);
-  
+
+  // After loading AdSpace Data, get the NFT owners on the contract
+  useEffect(() => {
+    if (AdSpace?.contract) {
+      fetchTokenOwners().then((data) => {
+        setTokenOwners(data);
+      });
+    }
+  }, [AdSpace]);
+
   // helper for select input
-  let optionTemplate = userCampaigns.map((v,index) => (
-    <option value={v.campaign_id} key={index}>{v.name}</option>
+  let optionTemplate = userCampaigns.map((v, index) => (
+    <option value={v.campaign_id} key={index}>
+      {v.name}
+    </option>
   ));
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
@@ -252,7 +293,13 @@ export default function AdSpaceListing() {
                 </FormControl>
                 <FormControl isRequired mt={4}>
                   <FormLabel>Campaign</FormLabel>
-                  <Select placeholder="Select campaign" isRequired={true} onChange={(e) => {setSelectedCampaign(e.target.value)}}>
+                  <Select
+                    placeholder="Select campaign"
+                    isRequired={true}
+                    onChange={(e) => {
+                      setSelectedCampaign(e.target.value);
+                    }}
+                  >
                     {optionTemplate}
                   </Select>
                 </FormControl>
@@ -280,8 +327,8 @@ export default function AdSpaceListing() {
                   >
                     Seal the Deal
                   </Button>
-                  ): (
-                    <Button
+                ) : (
+                  <Button
                     colorScheme="brand"
                     variant="solid"
                     onClick={() => {
@@ -290,7 +337,7 @@ export default function AdSpaceListing() {
                   >
                     Approve DAI
                   </Button>
-                  )}
+                )}
               </ModalFooter>
             </ModalContent>
           </Modal>
@@ -298,6 +345,54 @@ export default function AdSpaceListing() {
       ) : (
         <Text>AdSpace #{adspaceId} not found.</Text>
       )}
+      <Card
+        mb={{ base: "0px", "2xl": "20px" }}
+        mt={{ base: "20px", "2xl": "20px" }}
+      >
+        <Text
+          color={textColorPrimary}
+          fontWeight="bold"
+          fontSize="xl"
+          mt="10px"
+          mb="4px"
+        >
+          NFT Holders of {AdSpace?.name} (Total: {tokenOwners?.length})
+        </Text>
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Address</Th>
+                <Th isNumeric>Equity</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {tokenOwners.map((val) => {
+                return (
+                  <Tr>
+                    {val === address ? (
+                      <Td fontWeight="bold" color={"#3311DB"}>
+                        {val} <WalletIcon />
+                      </Td>
+                    ) : (
+                      <Td>{val}</Td>
+                    )}
+                    {val === address ? (
+                      <Td isNumeric fontWeight="bold" color={"#3311DB"}>
+                        {(100 / tokenOwners.length).toFixed(2)}%
+                      </Td>
+                    ) : (
+                      <Td isNumeric>
+                        {(100 / tokenOwners.length).toFixed(2)}%
+                      </Td>
+                    )}
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Card>
     </Box>
   );
 }
